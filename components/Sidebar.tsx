eab85cf07014b41fd845fa6843ccc186a9f192d8
shazab-saifi <shazebsaifi92@gmail.com>
2025-03-06 15:07:54 +0530

    Made a small change in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import periskope from '@/public/periskope.png';
 
 import { IoMdHome } from "react-icons/io";
-import { IoGitNetworkOutline } from "react-icons/io5"; 
+import { IoGitNetworkOutline } from "react-icons/io5";
 import { BsFillChatDotsFill } from "react-icons/bs";
 import { BiSolidCoupon } from "react-icons/bi";
 import { FaChartLine } from "react-icons/fa";
@@ -27,43 +27,37 @@ const Sidebar = () => {
     { icon: MdOutlineChecklist },
     { icon: TbSettingsFilled }
   ];
-
-  const bottomIcons = [
-    { icon: TbStarsFilled },
-    { icon: TbLayoutSidebarLeftExpandFilled }
-  ];
-
+  
   return (
     <aside className="p-4 h-screen flex flex-col items-center justify-between">
       <div className="flex flex-col items-center space-y-7">
-        <Image 
-          src={periskope} 
-          alt="profile" 
-          width={32} 
-          height={32} 
+        <Image
+          src={periskope}
+          alt="profile"
+          width={32}
+          height={32}
           className="rounded-full"
         />
         <div className="space-y-5">
           {menuIcons.map(({ icon: Icon, fixedColor, extraClass }, index) => (
-            <Icon 
+            <Icon
               key={index}
-              className={`cursor-pointer transition-colors duration-200 ${
-                fixedColor ? "" : "hover:text-[#0C8F4E]"
-              } ${extraClass || ""}`} 
-              size={24} 
-              style={fixedColor ? { color: fixedColor } : {}}
+              className={`cursor-pointer transition-colors duration-200 ${fixedColor ? "" : "hover:text-[#0C8F4E]"
+                } ${extraClass || ""}`}
+              size={24}
             />
           ))}
         </div>
       </div>
       <div className="space-y-5 opacity-60">
-        {bottomIcons.map(({ icon: Icon }, index) => (
-          <Icon 
-            key={index}
-            className="cursor-pointer transition-colors duration-200 hover:text-[#0C8F4E]" 
-            size={24} 
-          />
-        ))}
+        <TbStarsFilled
+          className="cursor-pointer transition-colors duration-200 hover:text-[#0C8F4E]"
+          size={24}
+        />
+        <TbLayoutSidebarLeftExpandFilled
+          className="cursor-pointer transition-colors duration-200 hover:text-[#0C8F4E]"
+          size={24}
+        />
       </div>
     </aside>
   );
